23422df7dc4fc7c1c622202bfa736f6b0ba4dc28
Terese Haimberger <haimberger@fitanalytics.com>
2019-01-27 20:35:20 +0100

    implemented basic task listing
    - added example list of tasks to data directory
    - show completed/active tasks or pending tasks based on `pending` query parameter
    - just display titles for now

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const data = require('../data/example.json');
+
 // GET task listing.
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  const showPending = req.query.pending === 'true';
+  const tasks = data.tasks.filter(function(task) {
+    const isPending = !task.isCompleted && task.id != data.activeTaskID;
+    return showPending === isPending;
+  });
+  res.render('tasks', { tasks });
 });
 
 module.exports = router;
