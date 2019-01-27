fbbc9575e6c89e384a049d343928b943abce39c1
Terese Haimberger <haimberger@fitanalytics.com>
2019-01-27 18:42:29 +0100

    adjustments to automatically generated app skeleton
    - replaced var with const
    - removed some superfluous comments
    - replaced /users with /tasks
    - specified Node.js version in package.json
    - removed support for named pipes

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const createError = require('http-errors');
+const logger = require('morgan');
+const path = require('path');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const taskRouter = require('./routes/tasks');
 
-var app = express();
+const app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -20,7 +19,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/tasks', taskRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
